8b5546de39a5a5eb1a5558df0c54538e7a2fbd9b
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-01 15:56:41 +0430

    add: fuzz package

---
+++ fuzz/fuzz.py
@@ -0,0 +1,12 @@
+"""
+Expert fuzzy system class.
+    - this class has parameters that each parameter has a range and
+    a function.
+    - you can add parameters with name.
+        - for each parameter, you can set a range.
+        - for each parameter, you can set 2 base functions.
+        - for each parameter, you can set 2 out of range values for left and right.
+    - you can remove parameters.
+"""
+class EFS:
+    pass
---
+++ fuzzification.py
@@ -0,0 +1,3 @@
+from fuzz import EFS
+
+# TODO: create a EFS system and set the values from our description
