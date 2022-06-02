858a4049152ff97f98a20614ccf91560ff3fb700
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-02 15:12:05 +0430

    parameter class added

---
+++ parameter/parameter.py
@@ -0,0 +1,13 @@
+"""
+Parameter class.
+    - fields:
+        - range: type list [begin, end]
+        - base function left (a, b)
+        - base function right (a , b)
+        - right range value
+        - left range value
+
+"""
+class Parameter:
+    def __init__(self):
+        pass
