ddfb86e225e88eb8a1b46d5c53a81ae529f6864c
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-03 12:17:08 +0430

    update: difuzzification

--- defuzzification.py
+++ defuzzification.py
@@ -1,18 +1,24 @@
 from scipy import ndimage
 import numpy as np
 
-masses = np.array(
-    [
-        [0,  0,  0,  0],
-        [0,  1,  0,  0],
-        [0,  2,  0,  0],
-        [1,  0,  0,  0],
-        [1,  1,  0,  1],
-        [1,  2,  0,  1],
-        [2,  0,  0,  0],
-        [2,  1,  0,  0],
-        [2,  2,  0,  0]
-    ]
-)
+def calculate(input):
+    masses = np.array(
+        [
+            [0,  0,  0,  0],
+            [0,  1,  0,  0],
+            [0,  2,  0,  0],
+            [1,  0,  0,  0],
+            [1,  1,  0,  1],
+            [1,  2,  0,  1],
+            [2,  0,  0,  0],
+            [2,  1,  0,  0],
+            [2,  2,  0,  0]
+        ]
+    )
 
-print(ndimage.measurements.center_of_mass(masses))
+    print(ndimage.measurements.center_of_mass(masses))
+
+
+def defuzzify(input):
+    # get the results and create the output
+    pass
