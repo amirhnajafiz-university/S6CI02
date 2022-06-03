e0b10a16ff0552bd08925c6aae4a7cb01bed14d3
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-03 12:02:34 +0430

    update: EFS implemented

--- fuzz/fuzz.py
+++ fuzz/fuzz.py
@@ -11,13 +11,9 @@ from parameter import Parameter
 
 """
 Expert fuzzy system class.
-    - this class has parameters that each parameter has a range and
-    a function.
-    - you can add parameters with name.
-        - for each parameter, you can set a range.
-        - for each parameter, you can set 2 base functions.
-        - for each parameter, you can set 2 out of range values for left and right.
-    - you can remove parameters.
+    - you can add parameters with name
+    - you can remove parameters
+    - you can get parameters
 """
 class EFS:
     def __init__(self):  # constructor
--- fuzz/fuzz_test.py
+++ fuzz/fuzz_test.py
@@ -7,11 +7,12 @@ from fuzz import EFS
 
 f = EFS()
 
-f.NewParam('test').Range([0, 2, 1]).LeftValue(0).RightValue(0)
+f.NewParam('test').NewSession('test1').Range([0, 2, 1]).LeftValue(0).RightValue(0)
+f.Param('test').NewSession('test2').Range([1, 3, 2]).LeftValue(0).RightValue(0)
 print(simplejson.dumps(f.Info(), sort_keys=True, indent=4, use_decimal=True))
 
-print(f.Param('test').Input(12) == 0)    # expect 0
-print(f.Param('test').Input(-1) == 0)    # expect 0
-print(f.Param('test').Input(1) == 1)     # expect 1
-print(f.Param('test').Input(0.5) == 0.5)   # expect 0.5
-print(f.Param('test').Input(1.5) == 0.5)   # expect 0.5
+print(f.Param('test').Input(12))    # expect 0, 0
+print(f.Param('test').Input(-1))    # expect 0, 0
+print(f.Param('test').Input(1))     # expect 1, 0
+print(f.Param('test').Input(0.5))   # expect 0.5, 0
+print(f.Param('test').Input(1.5))   # expect 0.5, 0,5
