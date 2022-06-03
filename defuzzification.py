588866604dcc4debebf55b3e88a084ae3044aed5
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-03 13:56:33 +0430

    output values added

--- defuzzification.py
+++ defuzzification.py
@@ -1,5 +1,16 @@
+from parameter.parameter import Parameter
 from scipy import ndimage
 import numpy as np
+
+
+# our output parameter
+output = Parameter()
+output.NewSession('healthy').Range([0.25, 1, 0.25]).LeftValue(1)
+output.NewSession('sick1').Range([0, 2, 1])
+output.NewSession('sick2').Range([1, 3, 2])
+output.NewSession('sick3').Range([2, 4, 3])
+output.NewSession('sick4').Range([3, 3.75, 3.75]).RightValue(1)
+
 
 def calculate(input):
     masses = np.array(
--- fuzzification.py
+++ fuzzification.py
@@ -9,48 +9,48 @@ def initialize():
     # our parameters
     # -----
     f.NewParam('chest_pain').NewSession('typical_anginal').Range([1, 1, 1])
-    f.With('chest_pain').NewSession('atypical_anginal').Range([2, 2, 2])
-    f.With('chest_pain').NewSession('non_aginal_pain').Range([3, 3, 3])
-    f.With('chest_pain').NewSession('asymptomatic').Range([4, 4, 4])
+    f.Param('chest_pain').NewSession('atypical_anginal').Range([2, 2, 2])
+    f.Param('chest_pain').NewSession('non_aginal_pain').Range([3, 3, 3])
+    f.Param('chest_pain').NewSession('asymptomatic').Range([4, 4, 4])
 
     f.NewParam('sex').NewSession('male').Range([0, 0, 0])
-    f.With('sex').NewSession('female').Range([1, 1, 1])
+    f.Param('sex').NewSession('female').Range([1, 1, 1])
 
     f.NewParam('exercise').NewSession('true').Range([1, 1, 1])
-    f.With('exercise').NewSession('false').Range([0, 0, 0])
+    f.Param('exercise').NewSession('false').Range([0, 0, 0])
 
     f.NewParam('age').NewSession('young').Range([29, 38, 29]).LeftValue(1)
-    f.With('age').NewSession('mid').Range([33, 45, 38])
-    f.With('age').NewSession('old').Range([40, 58, 48])
-    f.With('age').NewSession('veryold').Range([52, 60, 60]).RightValue(1)
+    f.Param('age').NewSession('mid').Range([33, 45, 38])
+    f.Param('age').NewSession('old').Range([40, 58, 48])
+    f.Param('age').NewSession('veryold').Range([52, 60, 60]).RightValue(1)
 
     f.NewParam('blood_pressure').NewSession('low').Range([111, 134, 111]).LeftValue(1)
-    f.With('blood_pressure').NewSession('medium').Range([127, 153, 139])
-    f.With('blood_pressure').NewSession('high').Range([142, 172, 157])
-    f.With('blood_pressure').NewSession('veryhigh').Range([154, 171, 171]).RightValue(1)
+    f.Param('blood_pressure').NewSession('medium').Range([127, 153, 139])
+    f.Param('blood_pressure').NewSession('high').Range([142, 172, 157])
+    f.Param('blood_pressure').NewSession('veryhigh').Range([154, 171, 171]).RightValue(1)
 
     f.NewParam('blood_sugar').NewSession('veryhigh').Range([105, 120, 120]).RightValue(1)
 
     f.NewParam('cholesterol').NewSession('low').Range([151, 197, 151]).LeftValue(1)
-    f.With('cholesterol').NewSession('medium').Range([188, 250, 215])
-    f.With('cholesterol').NewSession('high').Range([217, 307, 263])
-    f.With('cholesterol').NewSession('veryhigh').Range([281, 347, 347]).RightValue(1)
+    f.Param('cholesterol').NewSession('medium').Range([188, 250, 215])
+    f.Param('cholesterol').NewSession('high').Range([217, 307, 263])
+    f.Param('cholesterol').NewSession('veryhigh').Range([281, 347, 347]).RightValue(1)
 
     f.NewParam('heart_rate').NewSession('low').Range([100, 141, 100]).LeftValue(1)
-    f.With('heart_rate').NewSession('medium').Range([111, 194, 152])
-    f.With('heart_rate').NewSession('high').Range([152, 210, 210]).RightValue(1)
+    f.Param('heart_rate').NewSession('medium').Range([111, 194, 152])
+    f.Param('heart_rate').NewSession('high').Range([152, 210, 210]).RightValue(1)
 
     f.NewParam('ecg').NewSession('normal').Range([0, 0.4, 0]).LeftValue(1)
-    f.With('ecg').NewSession('abnormal').Range([0.2, 1.4, 1])
-    f.With('ecg').NewSession('hypertrophy').Range([1.4, 1.9, 1.9]).RightValue(1)
+    f.Param('ecg').NewSession('abnormal').Range([0.2, 1.4, 1])
+    f.Param('ecg').NewSession('hypertrophy').Range([1.4, 1.9, 1.9]).RightValue(1)
 
     f.NewParam('old_peak').NewSession('low').Range([1, 2, 1]).LeftValue(1)
-    f.With('old_peak').NewSession('risk').Range([1.5, 4.2, 2.8])
-    f.With('old_peak').NewSession('terrible').Range([2.5, 4.1, 4.1]).RightValue(1)
+    f.Param('old_peak').NewSession('risk').Range([1.5, 4.2, 2.8])
+    f.Param('old_peak').NewSession('terrible').Range([2.5, 4.1, 4.1]).RightValue(1)
 
     f.NewParam('thallium_scan').NewSession('normal').Range([3, 3, 3])
-    f.With('thallium_scan').NewSession('medium').Range([6, 6, 6])
-    f.With('thallium_scan').NewSession('high').Range([7, 7, 7])
+    f.Param('thallium_scan').NewSession('medium').Range([6, 6, 6])
+    f.Param('thallium_scan').NewSession('high').Range([7, 7, 7])
     # -----
 
     return f
